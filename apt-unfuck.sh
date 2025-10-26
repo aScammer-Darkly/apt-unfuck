533dd89ad40f680af5950e7824daf81f793a5801
Dumpster Wizard <probablyscam@dumpsterwizard.enterprises>
2025-10-26 19:28:22 +0000

    Remove unnecessary lines and add killall command

--- apt-unfuck.sh
+++ apt-unfuck.sh
@@ -1,6 +1,4 @@
-bash 
-#!/bin/bash
-set -e 
+sudo killall apt apt-get aptitde synaptic dpkg
 sudo rm -rf /var/lib/dpkg/lock
 sudo rm -rf /var/lib/apt/lists/lock
 sudo rm -rf /var/cache/apt/archives/lock
